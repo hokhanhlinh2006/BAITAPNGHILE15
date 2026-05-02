e57290a2cba66d010b2df7e5de910d9f04025e82
Ho Khanh Linh <hokhanhlinh2006@gmail.com>
2026-05-02 21:30:08 +0700

    first commit

--- src/main/java/com/example/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/example/employeemanagement/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.example.employeemanagement.controller;
 
-import com.example.employeemanagement.entity.*;
-import com.example.employeemanagement.repository.*;
+import com.example.employeemanagement.entity.Employee;
+import com.example.employeemanagement.repository.DepartmentRepository;
+import com.example.employeemanagement.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -24,8 +26,42 @@ public class EmployeeController {
     private final String UPLOAD_DIR = "uploads/";
 
     @GetMapping("/")
-    public String list(Model model) {
-        model.addAttribute("employees", employeeRepository.findAll());
+    public String list(
+            Model model,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size,
+            @RequestParam(defaultValue = "name") String sortField,
+            @RequestParam(defaultValue = "asc") String sortDir,
+            @RequestParam(required = false) String keyword
+    ) {
+
+        Sort sort = sortDir.equals("asc") ?
+                Sort.by(sortField).ascending() :
+                Sort.by(sortField).descending();
+
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        Page<Employee> employeePage;
+
+        if (keyword != null && !keyword.isEmpty()) {
+            employeePage = employeeRepository
+                    .findByNameContainingIgnoreCase(keyword, pageable);
+        } else {
+            employeePage = employeeRepository.findAll(pageable);
+        }
+
+        // DATA
+        model.addAttribute("employees", employeePage.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", employeePage.getTotalPages());
+        model.addAttribute("totalItems", employeePage.getTotalElements());
+
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+
+        model.addAttribute("keyword", keyword);
+
         return "list";
     }
 
--- src/main/java/com/example/employeemanagement/repository/EmployeeRepository.java
+++ src/main/java/com/example/employeemanagement/repository/EmployeeRepository.java
@@ -1,7 +1,12 @@
 package com.example.employeemanagement.repository;
 
 import com.example.employeemanagement.entity.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository<Employee> extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Page<Employee> findByNameContainingIgnoreCase(String name, Pageable pageable);
+
 }
